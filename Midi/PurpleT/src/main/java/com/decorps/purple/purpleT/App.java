e0e2dc367a5fb44ac1c1e9aa0d943ae0779c560c
ldecorps <ldecorps@gmail.com>
2012-07-22 11:31:36 +0100

    fist commit

---
+++ Midi/PurpleT/src/main/java/com/decorps/purple/purpleT/App.java
@@ -0,0 +1,13 @@
+package com.decorps.purple.purpleT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
